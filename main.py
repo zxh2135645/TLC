2e303aadc20f94e765c161e00f47e578e00b637c
James zhang <zxh2135645@gmail.com>
2018-06-18 22:50:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-        from .utils import get_msid, get_mseid, create_tlc_workflow
+from .tlc create_tlc_workflow
         
-        wf = create_tlc_workflow(config,
-                                 t1,
-                                 freesurf_parc,
-                                 flair_lesion
-                                 )
+"""
+An example for using TLC. With one configuration options, input image from T1 weighted and freesurfer parcellation masks,
+as well as the editted binary lesion mask based on FLAIR image. 
 
-        wf.config = {"execution": {"crashdump_dir": os.path.join(config["crash_directory"], t1_filename + self.flag)}}
-        wf.run(plugin=self.inputs.plugin,
-               plugin_args=self.inputs.plugin_args)
+Inputs::
+    config: Dictionary with configuration options. You can create your own
+    t1_file: full path of t1 image
+    freesurf_parc: full path of aparc+aseg.mgz from freesurfer
+    flair_lesion: editted binary lesion mask based on FLAIR image (can also be labeled)
+    
+Outputs::
+   nipype.pipeline.engine.Workflow object
+   
+"""
+        
+wf = create_tlc_workflow(config,
+                         t1,
+                         freesurf_parc,
+                         flair_lesion
+                        )
+wf.config = {"execution": {"crashdump_dir": os.path.join(config["crash_directory"], t1_filename + self.flag)}}
+wf.run(plugin=self.inputs.plugin,
+       plugin_args=self.inputs.plugin_args)
